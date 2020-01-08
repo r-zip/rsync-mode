aa1339c15d282707fbb343367bac1678cf33bfce
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2020-01-08 11:51:55 -0500

    Revert "save strings instead of buffer objects"
    This reverts commit 0855bb421ba1cc8e740194b257a97ee031d5c3d1.

--- rsync-mode.el
+++ rsync-mode.el
@@ -107,8 +107,7 @@
           (rsync-get-hostname remote-path)))
 
 (defun rsync--process-exit-hook (proc event)
-  (when-let* ((cur-buffer (get-buffer rsync--buffer-list))
-              (buffer-name (buffer-name cur-buffer)))
+  (when-let ((cur-buffer (get-buffer rsync--buffer-list)))
     (with-current-buffer cur-buffer
       (spinner-stop rsync--spinner)
       (setq rsync--buffer-list (cdr rsync--buffer-list))))
@@ -118,7 +117,7 @@
 
 (defun rsync-run (remote-path excludes local-path &optional dry-run)
   (rsync--start-spinner)
-  (push 'rsync--buffer-list (buffer-name (current-buffer)))
+  (push 'rsync--buffer-list (current-buffer))
   (setq rsync--process
         (start-process-shell-command
          "rsync"
