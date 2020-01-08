2339c5221f9dce822dfb7b2b7aa6b90a27a61211
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2020-01-08 14:07:10 -0500

    add documentation, attempt to consolidate minor mode logic

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # rsync-mode
-rsync projects to remote machines in Emacs
+
+`rsync-mode` is a minor mode to automatically rsync entire projects to
+remote hosts in Emacs.
+
+## Project configuration
+To specify remote hosts for the project, set the directory-local
+variables `rsync-remote-paths`, `rsync-local-path`, and
+`rsync-excluded-dirs` at the project root level. Example:
+
+``` emacs-lisp
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((rsync-remote-paths . ("host1:projects/" "host2:projects/"))
+         (rsync-local-path . "/path/to/local/project")
+         (rsync-excluded-dirs . (".git"
+                                 "data"
+                                 ".ipynb_checkpoints"
+                                 ".pytest_cache"
+                                 "venv"
+                                 "*.egg-info")))))
+```
+
+## Commands
+Two functions are defined for interactive use: `rsync` and
+`rsync-all`. The former synchronizes files to a single remote, which
+is chosen interactively using `ivy`, with completions read from the
+`rsync-remote-paths` directory-local variable. Integration with
+`helm`, `ido`, and the default `completing-read` is planned.
+
+The function `rsync-all` synchronizes the current project to all
+remote hosts found in the directory-local variable
+`rsync-remote-paths`.
+
+## `rsync-mode`
--- rsync-mode.el
+++ rsync-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Ryan Pilgrim <ryan.z.pilgrim@gmail.com>
 ;; URL: https://github.com/r-zip/rsync-mode.el
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "26.1") (spinner "1.7.3"))
+;; Package-Requires: ((emacs "26.1") (spinner "1.7.3") (ivy "0.13.0") (counsel "0.13.0"))
 ;; Keywords: remote rsync
 
 ;; rsync-mode requires at least GNU Emacs 26.3 and rsync 3.1.3,
@@ -34,29 +34,26 @@
 ;;; Code:
 
 (require 'spinner)
+(require 'ivy)
+(require 'counsel)
 (require 'time-stamp)
 
-(defvar rsync-local-path nil)
-(defvar rsync-remote-paths nil)
+(defvar rsync-local-path nil
+  "Local path to the project, as a string.")
+(defvar rsync-remote-paths nil
+  "Remote paths to the project as a list of strings.
+Each path should have the form 'host:/path/to/project'.")
 (defvar rsync-excluded-dirs nil)
 ;; to override, delete entry and save in dir-locals
-(defvar rsync-default-excluded-dirs '("data"
-                                      ".dvc"
-                                      ".idea"
-                                      ".git"
-                                      ".ipynb_checkpoints"
-                                      "output"
-                                      ".pytest_cache"
-                                      "venv"
-                                      ".vscode"
-                                      "*.egg-info"
-                                      ".venv"
-                                      "__pycache__"))
+(defcustom rsync-default-excluded-dirs nil
+  "List of directories to exclude from all projects for rsync.")
 
 
-(defvar rsync--process-exit-hook nil)
+(defvar rsync--process-exit-hook nil
+  "Closure defining the process cleanup code.")
 (defconst rsync--lighter
-  '(" rsync" (:eval (spinner-print rsync--spinner))))
+  '(" rsync" (:eval (spinner-print rsync--spinner)))
+  "The mode lighter for `rsync-mode'.")
 
 (defun rsync--start-spinner ()
   "Create and start a spinner on this buffer."
@@ -71,20 +68,25 @@
   ;; The indicator for the mode line
   :lighter rsync--lighter
   :group 'rsync
-  (if (not rsync-mode)
-      (remove-hook 'after-save-hook #'rsync-all t)
-    ;; taken from the spinner readme: https://github.com/Malabarba/spinner.el
-    (defvar-local rsync--spinner nil)
-    (defvar-local rsync--process nil)
-    (add-hook 'after-save-hook #'rsync-all 100 t)))
+  (rsync-with-info
+   (when remote-paths
+     (if (not rsync-mode)
+         (remove-hook 'after-save-hook #'rsync-all t)
+       ;; taken from the spinner readme: https://github.com/Malabarba/spinner.el
+       (defvar-local rsync--spinner nil)
+       (defvar-local rsync--process nil)
+       (add-hook 'after-save-hook #'rsync-all 100 t)))))
 
-(defun rsync-get-hostname (path)
+(defun rsync--get-hostname (path)
+  "Get the hostname from the remote path PATH."
   (let ((user-and-hostname (car (split-string path ":"))))
     (if (string-match-p "@" user-and-hostname)
         (cadr (split-string user-and-hostname "@"))
       user-and-hostname)))
 
-(defun rsync-get-excludes (excluded-dirs)
+(defun rsync--get-excludes (excluded-dirs)
+  "Get rsync exclude flags for EXCLUDED-DIRS.
+EXCLUDED-DIRS should be a list of strings."
   (if excluded-dirs
       (string-join
        (mapcar
@@ -95,21 +97,34 @@
     ""))
 
 (defmacro rsync-with-info (&rest forms)
+  "Load dir-local rsync information, then execute FORMS.
+When using this macro, `local-path', `remote-paths',
+`excluded-dirs', and `excludes' are available as local
+variables."
   (hack-dir-local-variables)
   `(when-let* ((local-path (alist-get 'rsync-local-path file-local-variables-alist))
                (remote-paths (alist-get 'rsync-remote-paths file-local-variables-alist))
                (excluded-dirs (alist-get 'rsync-excluded-dirs file-local-variables-alist))
-               (excludes (rsync-get-excludes excluded-dirs)))
+               (excludes (rsync--get-excludes excluded-dirs)))
      ,@forms))
 
 (defun rsync--get-rsync-buffer-name (remote-path)
+  "Generate the buffer name for the rsync process.
+REMOTE-PATH is the path to the rsync destination."
   (format "*rsync to %s*"
-          (rsync-get-hostname remote-path)))
+          (rsync--get-hostname remote-path)))
 
 (defun rsync--run-process-exit-hook (proc event)
+  "Run the closure defined by the variable `rsync--process-exit-hook'.
+PROC is the rsync process, which is present for call signature
+compatibility only. EVENT is the description of the event that
+changed the state of the rsync process."
   (funcall rsync--process-exit-hook proc event))
 
 (defun rsync--make-process-exit-hook (buffer)
+  "Create function to clean up the spinner for BUFFER.
+The created function will also message the user when the rsync
+process is complete and forward abnormal event strings."
   (lambda (proc event)
     (with-current-buffer buffer
       (spinner-stop rsync--spinner))
@@ -117,7 +132,11 @@
         (message "Rsync process received abnormal event %s" event)
       (message "Rsync complete."))))
 
-(defun rsync-run (remote-path excludes local-path &optional dry-run)
+(defun rsync--run (remote-path excludes local-path &optional dry-run)
+  "Synchronize the current project from LOCAL-PATH to REMOTE-PATH.
+Exclude according to EXCLUDES and the variable
+`rsync-default-excluded-dirs'. If DRY-RUN is t, call rsync with
+the dry-run flag."
   (rsync--start-spinner)
   (setq rsync--process
         (start-process-shell-command
@@ -137,13 +156,17 @@
   (set-process-sentinel rsync--process #'rsync--run-process-exit-hook))
 
 (defun rsync-all (&optional dry-run)
+  "Synchronize the current project to all remote hosts.
+If DRY-RUN is t, call rsync with the dry-run flag."
   (interactive)
   (rsync-with-info
    (when remote-paths
      (dolist (remote-path remote-paths)
-       (rsync-run remote-path excludes local-path dry-run)))))
+       (rsync--run remote-path excludes local-path dry-run)))))
 
-(defun rsync-select-remote (remote)
+(defun rsync--select-remote (remote)
+  "Interactively select the remote for synchronization.
+REMOTE is the selected remote host."
   (interactive
    (list
     (rsync-with-info
@@ -151,10 +174,14 @@
   remote)
 
 (defun rsync (&optional dry-run)
+  "Synchronize the current project to a single remote host.
+The host is selected interactively by the function
+`rsync--select-remote'. If DRY-RUN is t, call rsync with the
+dry-run flag."
   (interactive)
-  (let ((selected-remote (call-interactively #'rsync-select-remote)))
+  (let ((selected-remote (call-interactively #'rsync--select-remote)))
     (rsync-with-info
-     (rsync-run selected-remote excludes local-path dry-run))))
+     (rsync--run selected-remote excludes local-path dry-run))))
 
 (provide 'rsync-mode)
 ;;; rsync-mode.el ends here
